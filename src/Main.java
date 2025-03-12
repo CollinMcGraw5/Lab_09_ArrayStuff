0c4cf024a518bc4f3702728ae1264e9f38c5aa38
CollinMcGraw <mcgrawco@mail.uc.edu>
2025-03-12 15:32:25 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,84 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] dataPoints = new int[100];
+        Random rand = new Random();
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1;
+        }
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print(dataPoints[i]);
+            if (i < dataPoints.length - 1) {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println(); // New line for clarity
+
+        int sum = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            sum += dataPoints[i];
+        }
+        double average = (double) sum / dataPoints.length;
+
+        System.out.println("The sum of the random array dataPoints is: " + sum);
+        System.out.println("The average of the random array dataPoints is: " + average);
+
+        int userValue = SafeInput.getRangedInt("Enter an integer between 1 and 100: ", 1, 100);
+        System.out.println("You entered: " + userValue);
+
+        int count = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userValue) {
+                count++;
+            }
+        }
+        System.out.println("The value " + userValue + " was found " + count + " times in the dataPoints array.");
+
+        int searchValue = SafeInput.getRangedInt("Enter another integer between 1 and 100: ", 1, 100);
+        System.out.println("You entered: " + searchValue);
+
+        boolean found = false;
+        int position = -1;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == searchValue) {
+                found = true;
+                position = i;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("The value " + searchValue + " was found at array index " + position + ".");
+        } else {
+            System.out.println("The value " + searchValue + " was not found in the dataPoints array.");
+        }
+
+        int minValue = dataPoints[0];
+        int maxValue = dataPoints[0];
+        for (int i = 1; i < dataPoints.length; i++) {
+            if (dataPoints[i] < minValue) {
+                minValue = dataPoints[i];
+            }
+            if (dataPoints[i] > maxValue) {
+                maxValue = dataPoints[i];
+            }
+        }
+
+        // Output the minimum and maximum values
+        System.out.println("The minimum value in the dataPoints array is: " + minValue);
+        System.out.println("The maximum value in the dataPoints array is: " + maxValue);
+
+        System.out.println("Average of dataPoints is: " + getAverage(dataPoints));
+    }
+
+    public static double getAverage(int values[]) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return (double) sum / values.length;
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SafeInput {
+
+    public static int getRangedInt(String prompt, int low, int high) {
+        Scanner in = new Scanner(System.in);
+        int userInt;
+
+        while (true) {
+            System.out.print(prompt);
+            if (in.hasNextInt()) {
+                userInt = in.nextInt();
+                if (userInt >= low && userInt <= high) {
+                    break;
+                } else {
+                    System.out.println("Error: Please enter an integer between " + low + " and " + high + ".");
+                }
+            } else {
+                System.out.println("Error: Please enter a valid integer.");
+                in.next(); // clear invalid input
+            }
+        }
+        return userInt;
+    }
 }
